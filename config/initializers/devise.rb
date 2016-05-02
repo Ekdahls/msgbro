26bd984a5a3cb17ea3796eed288416ae2cd62ad3
Simon Ekdahl <simon_ekdahl@hotmail.com>
2016-05-02 17:12:47 +0200

    working mail app

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
 
 
-  config.mailer_sender = "mail-to-send@from.com"
+  config.mailer_sender = "noreply@msgbro.com"
   #config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
