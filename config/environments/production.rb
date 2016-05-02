be74d031375e6e403147a11a5f09c49873702a17
Simon Ekdahl <simon_ekdahl@hotmail.com>
2016-05-02 16:33:38 +0200

    This is a version with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app50117781@heroku.com'],
+  :password       => ENV['a4idfsxn9807'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
